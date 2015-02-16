ea7bbbf8f625352f1214abe64de1492584e65171
Riley Eynon-Lynch <riley@peardeck.com>
2015-02-15 18:53:59 -0600

    Clunky, manual connection between pure-java and GWT-exporter projects

--- CmereSharedJsBridge/src/us/larkoli/cmere/client/sharedJsBridge.java
+++ CmereSharedJsBridge/src/us/larkoli/cmere/client/sharedJsBridge.java
@@ -1,15 +1,24 @@
 package us.larkoli.cmere.client;
 
+import us.larkoli.cmere.shared.Card;
+
 import com.google.gwt.core.client.EntryPoint;
 
 public class sharedJsBridge implements EntryPoint {
 
+	private final Card card = Card.FOUR;
+
 	public void onModuleLoad() {
-		exportShared();
+		exportShared(card);
 	}
 
-	private native void exportShared() /*-{
-		$wnd.cmereShared = "omg so shared right now";
+	private native void exportShared(Card card) /*-{
+		var jsCard = card.@us.larkoli.cmere.shared.Card::toString()();
+
+		$wnd.cmereShared = {
+			test : "omg so shared right now",
+			card : jsCard
+		};
 		if (typeof $wnd.cmereSharedLoaded === 'function') {
 			$wnd.cmereSharedLoaded();
 		}
