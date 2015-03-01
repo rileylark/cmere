89cb2ba2ec30bf24b89c84ac0fc348a6c3c44e2d
Riley Eynon-Lynch <riley@peardeck.com>
2015-03-01 13:56:17 -0600

    Messing around

--- web/src/scripts/components/CmereApp.js
+++ web/src/scripts/components/CmereApp.js
@@ -1,28 +1,31 @@
 'use strict';
 
 var React = require('react/addons');
-var ReactTransitionGroup = React.addons.TransitionGroup;
-
-// Export React so the devtools can find it
-(window !== window.top ? window.top : window).React = React;
+var KnownHand = require('./KnownHand');
 
 // CSS
 require('../../styles/normalize.css');
 require('../../styles/main.css');
 
-var imageURL = require('../../images/yeoman.png');
 
 var CmereApp = React.createClass({
-  render: function() {
-    return (
-      <div className='main'>
-          <h1>HI!!!</h1>
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-        </ReactTransitionGroup>
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div className='main'>
+                <h1>C'mere</h1>
+                <div>
+                    Opponent Hand
+                </div>
+                <div>
+                    Stack
+                </div>
+                <div>
+                    Your Hand
+                    <KnownHand />
+                </div>
+            </div>
+            );
+    }
 });
 
 module.exports = CmereApp;
---
+++ web/src/scripts/components/KnownHand.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react/addons');
+
+// CSS
+require('../../styles/normalize.css');
+require('../../styles/main.css');
+
+
+var KnownHand = React.createClass({
+  render: function() {
+    return (
+      <div>CARDS</div>
+    );
+  }
+});
+
+module.exports = KnownHand;
