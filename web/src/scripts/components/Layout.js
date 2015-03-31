e1e50c96f7ce9890f08779c231ebd514f6bd5189
Riley Eynon-Lynch <riley@peardeck.com>
2015-03-31 18:05:48 -0500

    Use game state in multiple sibling widgets

--- web/src/scripts/components/Layout.js
+++ web/src/scripts/components/Layout.js
@@ -7,20 +7,51 @@ var GameBoard = require('./GameBoard');
 
 require('./layout.css');
 
+function checkForNewGameState(component) {
+    var gameState = component.props.gameStore.getGameState();
+
+    component.setState({
+        aGameExists: !!gameState,
+        menuOpen: !gameState || (gameState && gameState.gameOver)
+    });
+}
+
 module.exports = React.createClass({
-    render: function () {
+    componentWillMount: function () {
         var component = this;
+        component.props.eventBus.addListener(function (event) {
+            if (event.gameStateChanged) {
+                checkForNewGameState(component);
+            }
+        });
+
+        checkForNewGameState(component);
+    },
+    render: function () {
+        var component = this, gamePanel;
+
+
+        if (component.state.aGameExists) {
+            gamePanel = (
+                <GameBoard
+                gameStore={component.props.gameStore}
+                eventBus={component.props.eventBus}/>
+                );
+        } else {
+            gamePanel = <div>Start a New Game</div>;
+        }
+
+        var menuClass = component.state.menuOpen ? "menu--open" : "menu--closed";
+
         return <div>
             <div className="headerPanel">
                 <h1>C'mere</h1>
             </div>
-            <div className="menuPanel">
+            <div className={"menuPanel " + menuClass}>
                 <Menu eventBus={component.props.eventBus}/>
             </div>
-            <div className="gamePanel">
-                <GameBoard
-                gameStore={component.props.gameStore}
-                eventBus={component.props.eventBus}/>
+            <div className={"gamePanel " + menuClass}>
+            {gamePanel}
             </div>
 
         </div>
